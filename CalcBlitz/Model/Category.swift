d726972aa25d9adf987436d7fb2250a6abd7d2e4
Ben Do <bdo.appworkshop@gmail.com>
2026-03-02 13:28:55 -0600

    Added Category enum type.

---
+++ CalcBlitz/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  CalcBlitz
+//
+//  Created by Ben Do on 3/2/26.
+//
+
+
+enum Category: String, CaseIterable {
+    case Addition = "addition"
+    case Subtraction = "subtraction"
+    case Division = "division"
+    case Multiplication = "multiplication"
+}
