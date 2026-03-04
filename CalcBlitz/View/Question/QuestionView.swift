7b8e75991e52ae64235e322f46415af967040420
Ben Do <bdo.appworkshop@gmail.com>
2026-03-04 09:55:52 -0600

    Imported SwiftData to update the user score value to its data model.

--- CalcBlitz/View/Question/QuestionView.swift
+++ CalcBlitz/View/Question/QuestionView.swift
@@ -6,21 +6,45 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct QuestionView: View {
+    @Environment(\.modelContext) private var context: ModelContext
+    @Bindable var user: User
+    @State private var vm = QuestionViewModel()
+    
+    // Question Number variable
+    @State private var indexNum: Int = 1
+    
+    // Prompt variable
+    @State private var prompt = "1 + 1"
+    
+    // User's input
+    @State private var userInput: String = ""
+    
+    
     var body: some View {
         ZStack {
             VStack {
-                Text("Question 1")
-                    .font(.largeTitle)
-                Text("2 x 10:")
-                    .font(.title)
+                VStack {
+                    
+                    HStack {
+                        Text("\(indexNum))")
+                            .font(.largeTitle)
+                        Text("\(vm.questionModel.questions[indexNum].prompt)")
+                            .font(.largeTitle)
+                    }
                     .padding()
-                TextField("Answer", text: .constant(""))
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
+                    
+                    TextField("Answer", text: $userInput)
+                        
+                    
+                }
+                
+                
                 Button("Next") {
                     print("next question")
+                    self.nextQuestion()
                 }
                 .font(.title)
                 .buttonBorderShape(.automatic)
@@ -30,8 +54,32 @@ struct QuestionView: View {
         
         
     }
+    
+    func updateScore() {
+        // check if the user's input matches the answer
+        if (Int(userInput) == vm.questionModel.questions[indexNum].answer) {
+            user.score += 1
+        } else {
+            user.score -= 1
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func nextQuestion() {
+        // Change the question number
+        if indexNum < vm.questionModel.questions.count - 1 {
+            self.indexNum += 1
+            
+            prompt = vm.questionModel.questions[indexNum].prompt
+        }
+    }
 }
 
 #Preview {
-    QuestionView()
+    QuestionView(user: User(score: 0))
 }
