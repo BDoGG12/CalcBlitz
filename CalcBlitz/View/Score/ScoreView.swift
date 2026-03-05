593616e3dc41d691476dafc973906bec9ba0c66e
Ben Do <bdo.appworkshop@gmail.com>
2026-03-05 14:14:27 -0600

    Added skeleton layouts to ScoreView.

---
+++ CalcBlitz/View/Score/ScoreView.swift
@@ -0,0 +1,22 @@
+//
+//  ScoreView.swift
+//  CalcBlitz
+//
+//  Created by Ben Do on 3/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ScoreView: View {
+    var body: some View {
+        VStack {
+            Text("User Score: 0")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    ScoreView()
+}
